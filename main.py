ac15260eb20dc4cd2e025ce03b23541a82826bf0
Vadim Lunev <vadimlunev1908@gmail.com>
2022-10-07 17:03:00 +0500

    start commit

---
+++ main.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = []
+for i in range(n):
+    a.append(int(input()))
+for i in range(n-1):
+    for j in range(n-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+print(a)
